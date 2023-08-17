aa7140e4a2177d6f5b38f7b2b5def35edf8108ed
CYPARK <chanyoung990704@naver.com>
2023-08-17 13:56:12 +0900

    IntelliJ 환경 셋팅

--- nadocoding_machineLearning/nadocoding_intelliJ/practice1.py
+++ nadocoding_machineLearning/nadocoding_intelliJ/practice1.py
@@ -1,8 +1,13 @@
 # Linear Regression
 ## 공부 시간에 따른 시간 점수
-
+import warnings
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+from matplotlib import MatplotlibDeprecationWarning
+
+
+warnings.filterwarnings("ignore", category=MatplotlibDeprecationWarning)
 
 ##############선형 모델 학습 ##########################################################################
 
@@ -11,13 +16,12 @@ dataSet.head()
 
 # 독립 변수 설정
 
-X = dataSet.iloc[:, : -1].values # row, colunm //  : => 전부 다 ,  : -1 => 마지막 직전까지
+X = dataSet.iloc[:, : -1].values # row, column //  : => 전부 다 ,  : -1 => 마지막 직전까지
 
 # 종속 변수 설정
 Y = dataSet.iloc[:, -1].values
 
 
-from sklearn.linear_model import LinearRegression
 reg = LinearRegression() # 선형 모델 객체 생성
 reg.fit(X, Y) # 선형 모델 학습 (모델 생성)// fit => 학습 // 독립변수, 종속변수
 
@@ -26,7 +30,7 @@ print(predict_val)
 
 # 산점도
 plt.scatter(X, Y, color = 'blue')
-#선 그래프
+# 선 그래프
 plt.plot(X, predict_val, color = 'green')
 # 타이틀
 plt.title('Score by hours')
@@ -39,7 +43,7 @@ plt.show()
 
 
 ###
-print('9시간 공부 예상 점수는 ? ', reg.predict([    [9]      ])) # 입력했을 때 변수 자료형에 맞게 변수를 설정해야 
+print('9시간 공부 예상 점수는 ? ', reg.predict([    [9]      ])) # 입력했을 때 변수 자료형에 맞게 변수를 설정해야
 
 
 # 기울기 출력
@@ -48,11 +52,11 @@ print(reg.coef_)
 # y 절편
 print(reg.intercept_)
 
-###### => y = mx + b     => y = ( reg.coef_ )x + (reg.intercept_)
+# => y = mx + b     => y = ( reg.coef_ )x + (reg.intercept_)
 
 
 
-### 데이터 셋 분리 ######################################################
+# 데이터 셋 분리
 
 
 x_data = dataSet.iloc[: , : -1].values
@@ -88,7 +92,7 @@ reg.fit(x_train, y_train) ## 훈련 데이터 셋으로 모델 학습
 
 # 산점도
 plt.scatter(x_train, y_train, color = 'blue')
-#선 그래프
+# 선 그래프
 plt.plot(x_train, reg.predict(x_train), color = 'green')
 # 타이틀
 plt.title('Score by hours ( train set) ')
